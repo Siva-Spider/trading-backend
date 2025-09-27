0a50dd9b94c04b778dbb7a1b311fcfba9e2481af
Siva Prasad U <sivag.7916@gmail.com>
2025-09-27 21:55:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -328,9 +328,8 @@ def run_trading_logic_for_all(trading_parameters, selected_brokers,logger):
     import indicators as ind
 
     if not trading_parameters:
-        logger.write("❌ Trading loop aborted: Received empty trading parameters list.")
-        # Ensure logger is available globally or passed correctly
-        return # Exit the thread gracefully
+        LOG.waring("❌ Trading loop aborted: Received empty trading parameters list.")
+        return 
         
     # mark all as active initially
     for stock in trading_parameters:
